79c5f24fc6f4d4c451cd20ab1b85d2c5ab9d30b7
Amit Hauzer <amit.hauzer@gmail.com>
2023-01-19 20:14:57 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import Products from './components/Products';
+
+
+
 
 function App() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:8000/products/')
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data)
+        console.log(data)
+      })
+  }, [])
+
+
   return (
-    <div>
+    <>
       <h1>Hello</h1>
-    </div>
+
+
+      <Products products={products} />
+      {/* {products.map(p => <div>{p.name}</div>)} */}
+
+
+    </>
   );
 }
 
