e07167357676bb05ba98433ccb80405d2dcbb0fe
Amit Hauzer <amit.hauzer@gmail.com>
2023-01-28 21:28:56 +0200

    add home to route

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom'
 function Nav() {
   return (
     <div>
+        <Link to='/'>Home</Link>
+        <br/>
         <Link to='/products'>Products</Link>
         <br/>
         <Link to='/cart'>Cart</Link>
