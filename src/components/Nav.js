789cb1bb32bd8f9613295a67f5f1cf56d760f23a
Amit Hauzer <amit.hauzer@gmail.com>
2023-01-28 20:24:24 +0200

    init navbar

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Nav() {
+  return (
+    <div>
+        <Link to='/products'>Products</Link>
+        <br/>
+        <Link to='/cart'>Cart</Link>
+        <hr/>
+    </div>
+  )
+}
+
+export default Nav
