56bb3342adc0c11549562378794916494fc37b5b
Amit Hauzer <amit.hauzer@gmail.com>
2023-02-05 23:29:37 +0200

    h3=>h5

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,12 +1,11 @@
 import './MainHeader.css'
-import { Link } from 'react-router-dom'
 import Nav from './Nav'
 
 export const MainHeader = ({ index }) => {
   return (
     <div className='navbar'>
       <header>
-        <h3>EasyShop</h3>
+        <h5>EasyShop</h5>
       </header>
       <Nav index={index} />
     </div>
