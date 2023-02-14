37743b37e3d73d09fc3a76c1175c73593d6e81c4
Amit Hauzer <amit.hauzer@gmail.com>
2023-02-15 01:20:15 +0200

    search in products URL only

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,21 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Nav.css';
 import { BsCart3 } from 'react-icons/bs';
 import { Badge } from 'react-bootstrap';
 import Search from './Search';
 
 function Nav({ index, searchProducts, getProducts }) {
+  const location = useLocation();
+  const isProductUrl = location.pathname === '/products';
   return (
     <nav className='shadow-sm'>
       <ul>
         <li><Link to='/products' onClick={getProducts}>Products</Link></li>
-        <li ><Link to='/cart'><BsCart3 className='icon' /><Badge bg="secondary">{index}</Badge></Link></li>
+        <li ><Link to='/cart'><BsCart3 className='icon' /><Badge bg="warning">{index}</Badge></Link></li>
       </ul>
 
-      <div className="right">
-        {/* <div>{window.location.href.split('/').map(a => <span>{a}</span>)} && {path}/products </div> */}
-        <Search searchProducts={searchProducts} />
-
+      <div className="right" style={{ width: '20%' }}>
+        {isProductUrl && <Search searchProducts={searchProducts} />}
       </div>
     </nav >
   )
