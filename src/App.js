f5e0c3aabe7eeb5a6c2eb425e0be80820d5c6edd
Amit Hauzer <amit.hauzer@gmail.com>
2023-02-20 20:36:02 +0200

    add 404 page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Products from './components/Products';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Cart from './components/Cart';
 import { MainHeader } from './components/MainHeader';
+import { PageNotFound } from './components/PageNotFound';
 
 
 
@@ -57,6 +58,7 @@ function App() {
           <Route path='/' element={<Navigate to='/products' />} />
           <Route path='/products' element={<Products products={products} getCartItems={getCartItems} getProducts={getProducts} cartItems={cartItems} cartId={cartId} path={path} />} />
           <Route path='/cart' element={<Cart getCartItems={getCartItems} cartitems={cartItems} cartId={cartId} path={path} />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </>
