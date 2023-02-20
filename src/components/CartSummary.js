94f5768bc593b1795da2343775075bc5f93efb4a
Amit Hauzer <amit.hauzer@gmail.com>
2023-02-20 19:58:07 +0200

    add checkout

--- src/components/CartSummary.js
+++ src/components/CartSummary.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react"
 import { Button, Col, Row } from "react-bootstrap"
 import { FaShekelSign } from 'react-icons/fa';
+import { useNavigate } from "react-router-dom";
 
 
-export const CartSummary = ({ cartitems }) => {
+export const CartSummary = ({ cartitems, path, cartId, getCartItems }) => {
+    const navigate = useNavigate();
     const [totalPrice, setTotalPrice] = useState()
     const initialValue = 0;
 
@@ -11,6 +13,20 @@ export const CartSummary = ({ cartitems }) => {
         const newTotalPrice = cartitems.reduce((accumulator, item) => accumulator + item.product.price * item.quantity, initialValue);
         setTotalPrice(newTotalPrice)
     }, [cartitems])
+
+    const checkOut = async () => {
+        console.log('checkout')
+        await fetch(`${path}/orders/${cartId}/`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' }
+        })
+        getCartItems();
+        navigateToProducts();
+    }
+
+    const navigateToProducts = () => {
+        navigate("/products");
+    }
 
     return (
 
@@ -21,9 +37,9 @@ export const CartSummary = ({ cartitems }) => {
                 <div>Total Price:</div>
             </Col>
             <Col>
-                <div className="fw-bold">{parseFloat(totalPrice).toFixed(2)} <FaShekelSign size={'10px'} /></div>
+                <div className="fw-bold"><FaShekelSign size={'10px'} /> {parseFloat(totalPrice).toFixed(2)}</div>
             </Col>
-            <div className="d-grid pt-3"><Button className="shadow-sm" variant="success">Checkout</Button></div>
+            <div className="d-grid pt-3"><Button className="shadow-sm" variant="success" onClick={checkOut}>Checkout</Button></div>
         </Row >
 
     )
